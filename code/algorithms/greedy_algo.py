b951aa6d328e769f33d1674f004d95ae4a0e245c
VVoetz <wouter@DESKTOP-ENUVVFM>
2024-01-14 15:38:01 +0100

    probleem opgelost greedy_algo, assign_students van testalgo ge copy-paste om te kunnen runnen

--- code/algorithms/greedy_algo.py
+++ code/algorithms/greedy_algo.py
@@ -5,10 +5,10 @@ import math
 class Greedyalgo(object):
 
     def __init__(self, data):
-        self.courses = data.Courses
-        self.rooms = data.Rooms
-        self.students = data.Students
-        self.activities = data.Activities
+        self.Courses = data.Courses
+        self.Rooms = data.Rooms
+        self.Students = data.Students
+        self.Activities = data.Activities
     
 
     def run(self) -> None:
@@ -16,46 +16,44 @@ class Greedyalgo(object):
         total_activities = list()
 
         # Adds all necessary activities per course
-        for course in self.courses:
-            lectures = int(self.courses[course].num_hc)
-            expected = int(self.courses[course].expected)
+        for course in self.Courses:
+            lectures = int(self.Courses[course].num_hc)
+            expected = int(self.Courses[course].expected)
             for i in range(lectures):
                 id = "h" + str(i + 1)
                 test_act = activity.Activity(course, id, expected)
                 total_activities.append(test_act)
+                self.Courses[course].activity(test_act)
             
-            seminars = self.courses[course].num_wc
-            if self.courses[course].max_wc != "":
-                max = int(self.courses[course].max_wc)
-                test = math.ceil(int(self.courses[course].expected) / max)
+            seminars = self.Courses[course].num_wc
+            if self.Courses[course].max_wc != "":
+                max = int(self.Courses[course].max_wc)
+                test = math.ceil(int(self.Courses[course].expected) / max)
                 seminars = int(seminars) * int(test)
-                for j in range(int(seminars)):
-                    id = "w" + str(j + 1)
-                    test_act = activity.Activity(course, id, max)
-                    total_activities.append(test_act)
-            else:
-                expected = int(self.courses[course].expected)
-                id = "w" + str(1)
-                test_act = activity.Activity(course, id, expected)
+            for j in range(int(seminars)):
+                id = "w" + str(j + 1)
+                test_act = activity.Activity(course, id, max)
                 total_activities.append(test_act)
+                self.Courses[course].activity(test_act)
+            
 
-            practica = self.courses[course].num_pr
-            if self.courses[course].max_pr != "":
-                max = int(self.courses[course].max_pr)
-                test = math.ceil(int(self.courses[course].expected) / max)
+            practica = self.Courses[course].num_pr
+            if self.Courses[course].max_pr != "":
+                max = int(self.Courses[course].max_pr)
+                test = math.ceil(int(self.Courses[course].expected) / max)
                 practica = int(practica) * int(test)
-                for k in range(int(practica)):
-                    id = "p" + str(k + 1)
-                    test_act = activity.Activity(course, id, max)
-                    total_activities.append(test_act)
-            else:
-                expected = int(self.courses[course].expected)
-                id = "p" + str(1) 
-                test_act = activity.Activity(course, id, expected)
-                total_activities.append(test_act)    
+            for k in range(int(practica)):
+                id = "p" + str(k + 1)
+                test_act = activity.Activity(course, id, max)
+                total_activities.append(test_act)
+                self.Courses[course].activity(test_act) 
 
-        rooms = self.rooms.keys()
+
+        rooms = list()
+        for room in self.Rooms:
+            rooms.append(self.Rooms[room])
         assign_all(total_activities, rooms)
+        assign_students(self.Courses)
 
 def assign_all(activities, rooms: list) -> None:
     for activity in sorted(activities, key=lambda room: room.capacity):
@@ -65,23 +63,38 @@ def fill_smallest_room(activity, rooms: list) -> None:
     for room in sorted(rooms, key=lambda room: room.capacity):
         if activity.capacity>room.capacity:
             break
-        slots = rooms[room].return_availability()
+        slots = room.return_availability()
         if len(slots) > 0:
             chosen_slot = slots[0]
-            rooms[room].add_activity(activity, chosen_slot)
+            room.add_activity(activity, chosen_slot)
             activity.set_timeslot(chosen_slot)
-            activity.set_room(rooms[room])
+            activity.set_room(room)
             return 0
     #  if no room available that fits capacity, take largest available room to minimise 'maluspunten'   
     for room in sorted(rooms, key=lambda room: room.capacity, reverse=True):
-        slots = rooms[room].return_availability()
+        slots = room.return_availability()
         if len(slots) > 0:
             chosen_slot = slots[0]
-            rooms[room].add_activity(activity, chosen_slot)
+            room.add_activity(activity, chosen_slot)
             activity.set_timeslot(chosen_slot)
-            activity.set_room(rooms[room])
+            activity.set_room(room)
             return 1
 
+def assign_students(courses):
+    for course in courses:
+        for item in courses[course].activities:
+            if str(item.id)[0] == 'h':
+                for student in courses[course].students:
+                    student.add_activity(item)
+                    item.add_student(student)
+            if str(item.id[0]) == 'w':
+                for student in courses[course].students:
+                    student.add_activity(item)
+                    item.add_student(student)
+            if str(item.id[0]) == 'p':
+                for student in courses[course].students:
+                    student.add_activity(item)
+                    item.add_student(student)
 
 
 
