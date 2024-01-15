449e8f47dd4bbe83c385e293ead2ef3523f9993d
sunshine <sunshinelandvreugd17@gmail.com>
2024-01-15 11:55:31 +0100

    course class activities

--- code/classes/course.py
+++ code/classes/course.py
@@ -1,9 +1,9 @@
+from activity import Activity
+import math
+
 class Course(object):
 
-    def __init__(self, name: str, num_hc :str, num_wc: str, max_wc: str, num_pr: str, max_pr: str, expected: str) -> None:
-        """
-        Constructor for Course data-object
-        """
+    def __init__(self, name, num_hc, num_wc, max_wc, num_pr, max_pr, expected):
         self.name = name
         self.num_hc = num_hc
         self.num_wc = num_wc
@@ -13,23 +13,52 @@ class Course(object):
         self.expected = expected
         self.students = []
         self.activities = []
+
+        """
+        Loading activities
+        """
+        self.activities()
     
-    def register(self, studentnumber: str) -> None:
-        """
-        Adds given student number to this course
+    def activities(self):
+        lectures = int(self.num_hc)
+        expected = int(self.expected)
+        for i in range(lectures):
+            id = "h" + str(i + 1)
+            test_act = Activity(self.name, id, expected)
+            self.activities.append(test_act)
+        
+        seminars = self.num_wc
+        if self.max_wc != "":
+            max = int(self.max_wc)
+            test = math.ceil(int(self.expected) / max)
+            seminars = int(seminars) * int(test)
+            for j in range(int(seminars)):
+                id = "w" + str(j + 1)
+                test_act = Activity(self.name, id, max)
+                self.activities.append(test_act)
+        else:
+            expected = int(self.expected)
+            id = "w" + str(1)
+            test_act = Activity(self.name, id, expected)
+            self.activities.append(test_act)
 
-        pre:    student number is a string
-        """
+        practica = self.num_pr
+        if self.max_pr != "":
+            max = int(self.max_pr)
+            test = math.ceil(int(self.expected) / max)
+            practica = int(practica) * int(test)
+            for k in range(int(practica)):
+                id = "p" + str(k + 1)
+                test_act = Activity(self.name, id, max)
+                self.activities.append(test_act)
+        else:
+            expected = int(self.expected)
+            id = "p" + str(1) 
+            test_act = Activity(self.name, id, expected)
+            self.activities.append(test_act)    
 
+    def register(self, studentnumber):
         self.students.append(studentnumber)
-        pass
     
-    def activity(self, activity) -> None:
-        """
-        Adds given activity to this course
-
-        pre:    activity is an activity object
-        """
-
-        self.activities.append(activity)
-        pass
+    def activity(self, activiteit):
+        self.activities.append(activiteit)
