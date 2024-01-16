85013936b9c685ffa1f65ff7effa1d363c3f0745
Fyqtor <v.germans77@gmail.com>
2024-01-16 13:14:23 +0100

    added tabu template file

---
+++ code/algorithms/tabu_search_algo.py
@@ -0,0 +1,17 @@
+from code.classes import room, course, student
+from code.classes import activity
+import math, random
+
+class Tabu_search():
+    def __init__(self, data) -> None:
+        self.Courses = data.Courses
+        self.Rooms = data.Rooms
+        self.Students = data.Students
+        self.Activities = data.Activities
+    
+        # TODO
+        # create_initial_solution()
+        # run()
+    
+    def create_initial_solution(self) -> None:
+        pass
