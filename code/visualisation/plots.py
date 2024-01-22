f91142e235b449ea7c4c10cb96973517420bfe92
sunshine <sunshinelandvreugd17@gmail.com>
2024-01-22 13:03:57 +0100

    plots

--- code/visualisation/plots.py
+++ code/visualisation/plots.py
@@ -2,22 +2,61 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import csv
 
-def stacked_plot(categories: list, y1, y2, y3, y4, sim: int):
+
+def open_malus_csv(filename: str):
+    """
+    Writes csv files for malus points data
+    """
+    malus_room_capacity = list()
+    malus_fifth_slot = list()
+    malus_double_acts = list()
+    malus_single_gaps = list()
+    malus_double_gaps = list()
+    malus_triple_gaps = list()
+    maluslist = list()
+
+
+    with open(f'data/{filename}', mode='r') as file:
+        next(file)
+        csvFile = csv.reader(file)
+        for line in csvFile:
+            malus_room_capacity.append(line[0])
+            malus_fifth_slot.append(line[1])
+            malus_double_acts.append(line[2])
+            malus_single_gaps.append(line[3])
+            malus_double_gaps.append(line[4])
+            malus_triple_gaps.append(line[5])
+            maluslist.append(line[6])
+    
+    return [malus_room_capacity, malus_fifth_slot, malus_double_acts, malus_single_gaps, malus_double_gaps, malus_triple_gaps, maluslist]
+            
+
+
+def stacked_plot(categories: list, data):
     """
     Function plots stacked percentage of maluspoints per category
     """
     the_colors = sns.cubehelix_palette(4, start=0.5, rot=-.75)
-    simulations = sim + 1
-    simulations = range(1, simulations)
+
+    y1 = data[0]
+    y2 = data[1]
+    y3 = data[2]
+    y4 = data[3]
+    y5 = data[4]
+    y6 = data[5]
+
+    simulations = range(1, (len(y1)+1))
+
+
     # basic stacked area chart
-    plt.stackplot(simulations, y1, y2, y3, y4, labels=categories, colors=the_colors)
+    plt.stackplot(simulations, y1, y2, y3, y4, y5, y6, labels=categories, colors=the_colors)
 
     # define axes limits
-    plt.xlim([1, sim])
+    plt.xlim([1, len(y1)])
     plt.ylim([0, 1])
 
     # define axes labels
-    plt.ylabel('Percentage maluspunten per categorie %   ')
+    plt.ylabel('Percentage maluspunten per categorie %')
     plt.xlabel('Simulaties')
 
     # plot text & line on top of figure
@@ -33,14 +72,14 @@ def plot_hist(filename: str):
     """
     Function plots histogram
     """
-    x = []
+    data = []
     with open(f'{filename}.csv', mode='r') as file:
         next(file)
         csvFile = csv.reader(file)
         for lines in csvFile:
-            x.append(int(lines[0]))
+            data.append(int(lines[0]))
 
-    fig = sns.histplot(x, kde=True)
+    fig = sns.histplot(data, kde=True)
     fig.set(xlabel='Maluspunten', ylabel='Frequentie')
     plt.savefig("code/visualisation/malus_point_plot.png")
 
@@ -49,7 +88,7 @@ def plot_hist(filename: str):
     # plt.ylabel("Frequentie")
     # plt.savefig("code/visualisation/malus_point_plot.png")
 
-def circle_diagram(filename):
+def pie_chart(filename):
     """
     Prints circle diagram with average percentage of maluspoints per categorie
     """
